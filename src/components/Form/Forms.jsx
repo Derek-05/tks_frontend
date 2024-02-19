1898c6e07d51168ea4eb03bec41dbd3d08051f5d
Derek <dereksanchezrosas@hotmail.com>
2024-02-19 13:41:42 -0400

    fixed the form

--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -8,14 +8,13 @@ const Forms = ({ onFormSuccess }) => {
   const [credentials, setCredentials] = useState({
     first_name: "",
     last_name: "",
+    email: "",
     dof: "",
     gender: "",
-    email: "",
     phone_number: "",
     job_offering_id: "",
     file: null, // Store file object here
   });
-
   const [loading, setLoading] = useState(false);
   const [jobOfferings, setJobOfferings] = useState([]);
   const [selectedFileName, setSelectedFileName] = useState(""); // To display selected file name
@@ -48,7 +47,7 @@ const Forms = ({ onFormSuccess }) => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
+  
     try {
       const formData = new FormData();
       Object.entries(credentials).forEach(([key, value]) => {
@@ -58,10 +57,8 @@ const Forms = ({ onFormSuccess }) => {
           formData.append(key, value);
         }
       });
-
       // Call the newApplicant API function with form data
       const response = await newApplicant(formData);
-
       // Call the onFormSuccess callback with user data
       onFormSuccess(response.user);
     } catch (error) {
@@ -86,23 +83,6 @@ const Forms = ({ onFormSuccess }) => {
         [name]: value,
       }));
     }
-
-    // Limit input length for certain fields
-    const maxLength = {
-      first_name: 100,
-      last_name: 100,
-      email: 100,
-      phone_number: 15,
-    };
-
-    if (value.length > maxLength[name]) {
-      return; // Don't update state if character limit exceeded
-    }
-
-    setCredentials((prev) => ({
-      ...prev,
-      [name]: name === "gender" ? e.target.value : value,
-    }));
   };
 
   // Handle job selection change
@@ -112,127 +92,125 @@ const Forms = ({ onFormSuccess }) => {
   };
 
   return (
-    <body className="Apply-Form-body">
-      <section className="container">
-        <form onSubmit={handleFormSubmit} className="Newform">
-          <h1>Apply Now</h1>
-          <div className="input-box">
-            <label htmlFor="first_name">First Name</label>
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              placeholder="First Name"
-              value={credentials.first_name}
-              onChange={handleChange}
-              maxLength={100} // Limiting input length
-              required // Required attribute added
-            />
-          </div>
-          <div className="input-box">
-            <label htmlFor="last_name">Last Name</label>
-            <input
-              type="text"
-              id="last_name"
-              name="last_name"
-              placeholder="Last Name"
-              value={credentials.last_name}
-              onChange={handleChange}
-              maxLength={100} // Limiting input length
-              required // Required attribute added
-            />
-          </div>
+    <section className="container">
+      <form onSubmit={handleFormSubmit} className="Newform">
+        <h1>Apply Now</h1>
+        <div className="input-box">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            placeholder="First Name"
+            value={credentials.first_name}
+            onChange={handleChange}
+            maxLength={100} // Limiting input length
+            required // Required attribute added
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            placeholder="Last Name"
+            value={credentials.last_name}
+            onChange={handleChange}
+            maxLength={100} // Limiting input length
+            required // Required attribute added
+          />
+        </div>
 
-          <div>
-            <label htmlFor="dof">Date of Birth</label>
-            <input
-              type="date"
-              id="dof"
-              name="dof"
-              value={credentials.dof}
-              onChange={handleChange}
-              required // Required attribute added
-            />
-          </div>
-          <div className="input-box">
-            <label htmlFor="gender">Gender</label>
-            <select
-              id="gender"
-              name="gender"
-              placeholder="Select Gender"
-              value={credentials.gender}
-              onChange={handleChange}
-              required // Required attribute added
-            >
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
+        <div>
+          <label htmlFor="dof">Date of Birth</label>
+          <input
+            type="date"
+            id="dof"
+            name="dof"
+            value={credentials.dof}
+            onChange={handleChange}
+            required // Required attribute added
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="gender">Gender</label>
+          <select
+            id="gender"
+            name="gender"
+            placeholder="Select Gender"
+            value={credentials.gender}
+            onChange={handleChange}
+            required // Required attribute added
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
 
-          <div className="input-box">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="example@hotmail.com"
-              value={credentials.email}
-              onChange={handleChange}
-              maxLength={100}
-              required
-            />
-          </div>
+        <div className="input-box">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@hotmail.com"
+            value={credentials.email}
+            onChange={handleChange}
+            maxLength={100}
+            required
+          />
+        </div>
 
-          <div className="input-box">
-            <label htmlFor="phone_number">Phone Number</label>
-            <input
-              type="tel"
-              id="phone_number"
-              name="phone_number"
-              placeholder="7871234567"
-              value={credentials.phone_number}
-              onChange={handleChange}
-              maxLength={15}
-              required
-            />
-          </div>
+        <div className="input-box">
+          <label htmlFor="phone_number">Phone Number</label>
+          <input
+            type="tel"
+            id="phone_number"
+            name="phone_number"
+            placeholder="7871234567"
+            value={credentials.phone_number}
+            onChange={handleChange}
+            maxLength={15}
+            required
+          />
+        </div>
 
-          <div className="input-box">
-            <label>Job Offers</label>
-            <select
-              onChange={handleJobChange}
-              value={credentials.job_offering_id}
-            >
-              <option value="">Select Job</option>
-              {jobOfferings.map((job) => (
-                <option key={job.id} value={job.id}>
-                  {job.title}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="input-box">
-            <label htmlFor="resume">Resume</label>
-            <input
-              type="file"
-              name="file"
-              accept="application/pdf"
-              onChange={handleChange}
-              required
-            />
-            {selectedFileName && (
-              // Display selected file name if available
-              <p>Selected file: {selectedFileName}</p>
-            )}
-          </div>
-          {loading && <p>Loading...</p>}
-          <button type="submit" disabled={loading}>
-            {loading ? "Submitting..." : "Submit"}
-          </button>
-        </form>
-      </section>
-    </body>
+        <div className="input-box">
+          <label>Job Offers</label>
+          <select
+            onChange={handleJobChange}
+            value={credentials.job_offering_id}
+          >
+            <option value="">Select Job</option>
+            {jobOfferings.map((job) => (
+              <option key={job.id} value={job.id}>
+                {job.title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="input-box">
+          <label htmlFor="resume">Resume</label>
+          <input
+            type="file"
+            name="file"
+            accept="application/pdf"
+            onChange={handleChange}
+            required
+          />
+          {selectedFileName && (
+            // Display selected file name if available
+            <p>Selected file: {selectedFileName}</p>
+          )}
+        </div>
+        {loading && <p>Loading...</p>}
+        <button type="submit" disabled={loading}>
+          {loading ? "Submitting..." : "Submit"}
+        </button>
+      </form>
+    </section>
   );
 };
 
